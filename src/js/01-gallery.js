f26a11a03fe8533b47dbfd10172a289d917664b4
Lovchinskaya <katia.lovchinskaya@gmail.com>
2022-10-14 19:39:41 +0300

    HW7 1task not ready

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryMarkup = createGalleryImages(galleryItems);  
+
+const galleryContainer = document.querySelector('.gallery');
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryContainer.addEventListener('click', onClickImage);
+
+
+
+function onClickImage(event) {
+    event.preventDefault();
+        console.log('click');
+  //  if (event.target.tagName !== 'gallery__image') return;
+ 
+    const instance = basicLightbox.create(
+    `<div class="modal">
+      <img src="${event.target.dataset.source}" width="800" height="600">
+     </div>`      );  
+    instance.show();
+
+}
+//position: absolute;
+    /* opacity: 1; */
+ //   z-index: 2;
+    /* top: 8%; */
+    /* left: 17%; */
+    /* transform: translate(14%, -112%); */
+//    top: 5em;
+ //   left: 20em;
+
+function createGalleryImages(images) {
+    const markupImages = images.map(({preview,original,description }) => {
+        return `
+       <div class="gallery__item">
+  <a class="gallery__link" href="${preview}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    }).join('');
+    return markupImages;
+}
+
+
